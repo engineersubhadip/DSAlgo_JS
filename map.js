19479748e3fef69a323e9d70b571c9ff1620c5fe
Subhadip Das <subhadipdas911@gmail.com>
2023-08-17 11:33:05 +0530

    frequency Map

---
+++ map.js
@@ -0,0 +1,13 @@
+const array = [1,1,2,3,2];
+
+let hashmap = new Map();
+
+for (let i=0; i<array.length; i++){
+    if (hashmap.has(array[i]) === true){
+        hashmap.set(array[i],hashmap.get(array[i])+1);
+    }else{
+        hashmap.set(array[i],1);
+    }
+}
+
+console.log(hashmap);
