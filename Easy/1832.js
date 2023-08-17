168df654a6e0203ee3073745926e941a01719d9e
Subhadip Das <subhadipdas911@gmail.com>
2023-08-17 14:03:21 +0530

    testing : Code compilation successfull

--- Easy/1832.js
+++ Easy/1832.js
@@ -1,1 +1,17 @@
-let sentence = "thequickbrownfoxjumpsoverthelazydog";
+let sentence = "leetcode";
+
+const array = []
+
+for (let i=97; i<=122;i++){
+    let charValue = String.fromCharCode(i);
+    array.push(charValue);
+}
+
+for (let i=0; i<array.length;i++){
+    if (! sentence.includes(array[i])){
+        console.log(false);
+        break;
+    }
+}
+console.log(true);
+
