969bd079bb3ff0ca61eb6159d8332f3bb096db68
Subhadip Das <subhadipdas911@gmail.com>
2023-08-17 20:56:32 +0530

    testing : Code compilation successfull

--- Easy/howManySubarrays.js
+++ Easy/howManySubarrays.js
@@ -1,3 +1,25 @@
 // https://www.scaler.com/help_requests/809749/?ref=call-now
 
-// Given a String we need to find out in how many Subarrays a particular element is present
+// Given a String we need to find out in how many Subarrays a particular element is present.
+
+let string = "acdbccb";
+let stringLength = string.length;
+let total = Math.floor(stringLength * (stringLength+1)/2);
+let start = 0;
+let end = 0;
+
+
+
+while (end < string.length){
+    if (string[end] === 'b'){
+        let runningLength = end-start;
+        total -= Math.floor(runningLength * (runningLength+1)/2);
+        start = end+1;
+    }
+    end++;
+}
+
+let runningLength = end-start;
+total -= Math.floor(runningLength * (runningLength +1)/2);
+
+console.log(total);
