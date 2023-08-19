702e9ffbaee11d08ee218f3539075654f7f985bd
Subhadip Das <subhadipdas911@gmail.com>
2023-08-19 11:14:57 +0530

    fix: fixed prefix initialization

--- Easy/colorfulNumber.js
+++ Easy/colorfulNumber.js
@@ -1,4 +1,4 @@
-let A = 236;
+let A = 23;
 
 const array = [];
 
@@ -9,6 +9,39 @@ while (A>0){
 }
 
 array.reverse();
+
+const prefix = [];
+
+for (let i=0; i<array.length; i++){
+    prefix.push(1);
+}
+
+prefix[0] = array[0];
+
+for (let i=1; i<array.length; i++){
+    prefix[i] = prefix[i-1]*array[i];
+}
+
+let hashset = new Set();
+
+for (let i=0; i<array.length; i++){
+    let runningProduct = 0;
+    for (let j=i; j<array.length; j++){
+        if (i === 0){
+            runningProduct = prefix[j];
+        }else{
+            runningProduct = Math.floor(prefix[j]/prefix[i-1]);
+        }
+
+        if (hashset.has(runningProduct)){
+            console.log(false);
+        }else{
+            hashset.add(runningProduct);
+        }
+    }
+}
+
+console.log(true);
 
 
 
