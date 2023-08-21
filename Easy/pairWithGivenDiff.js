78f5751253a35691029549a735c22426a2242c56
Subhadip Das <subhadipdas911@gmail.com>
2023-08-21 12:03:58 +0530

    test: Code compilation success

--- Easy/pairWithGivenDiff.js
+++ Easy/pairWithGivenDiff.js
@@ -6,25 +6,60 @@
 
 //! Approach -01: TC:O(NlogN); SC: O(1);
 
-const A = [-100, 20, 80];
-let B = 9;
+// const A = [-100, 20, 80];
+// let B = 9;
+
+// let hashmap = new Map();
+// A.sort((a,b) => a-b);
+
+// let iPointer = 0;
+// let jPointer = 0;
+
+// let flag = 0;
+
+// while (iPointer < A.length && jPointer < A.length){
+//     if (A[jPointer] - A[iPointer] === B && iPointer !== jPointer){
+//         flag = 1;
+//         break;
+//     }else if(A[jPointer] - A[iPointer] <= B){
+//         jPointer++;
+//     }else{
+//         iPointer++;
+//     }
+// }
+
+// console.log(flag);
+
+// ! Approach-02: TC:O(N); SC:O(N)
+
+const A = [5, 10, 3, 2, 50, 80];
+let B = 78;
 
 let hashmap = new Map();
-A.sort((a,b) => a-b);
 
-let iPointer = 0;
-let jPointer = 0;
-
-let flag = 0;
-
-while (iPointer < A.length && jPointer < A.length){
-    if (A[jPointer] - A[iPointer] === B && iPointer !== jPointer){
-        flag = 1;
-        break;
-    }else if(A[jPointer] - A[iPointer] <= B){
-        jPointer++;
+for (let i=0; i<A.length; i++){
+    if (hashmap.has(A[i])){
+        hashmap.set(A[i],hashmap.get(A[i])+1);
     }else{
-        iPointer++;
+        hashmap.set(A[i],1);
+    }
+}
+
+let flag = false;
+
+for (let i=0; i<A.length; i++){
+    let first = A[i];
+    let second = first-B;
+    if (hashmap.has(second)){
+        if (first === second){
+            if (hashmap.get(second) > 1){
+                flag = true;
+                break;
+            }
+        }else{
+            flag = true;
+            break;
+        }
     }
 }
 
