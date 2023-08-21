5350bd22621b884002519d5dde4f195e158ebfb6
Subhadip Das <subhadipdas911@gmail.com>
2023-08-21 11:44:14 +0530

    style: styled question parameters

--- Easy/pairWithGivenDiff.js
+++ Easy/pairWithGivenDiff.js
@@ -1,10 +1,10 @@
-// Given an one-dimensional unsorted array A containing N integers.
+//* Given an one-dimensional unsorted array A containing N integers.
 
-// You are also given an integer B, find if there exists a pair of elements in the array whose difference is B.
+//?  You are also given an integer B, find if there exists a pair of elements in the array whose difference is B.
 
-// Return 1 if any such pair exists else return 0.
+//? Return 1 if any such pair exists else return 0.
 
-// Approach -01: TC:O(NlogN); SC: O(1);
+//! Approach -01: TC:O(NlogN); SC: O(1);
 
 const A = [-100, 20, 80];
 let B = 9;
