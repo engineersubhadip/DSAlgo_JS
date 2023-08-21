586c13f0cb54be9e0ff62ee314428cd0d65dc801
Subhadip Das <subhadipdas911@gmail.com>
2023-08-21 12:09:35 +0530

    problem statement added

---
+++ Easy/countPairDiff.js
@@ -0,0 +1,4 @@
+//* You are given an array A of N integers and an integer B.
+//? Count the number of pairs (i,j) such that A[i] - A[j] = B and i ≠ j.
+//! Since the answer can be very large, return the remainder after dividing the count with 109+7.
+
