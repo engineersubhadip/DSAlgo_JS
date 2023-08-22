ea5cb4d6d5ac6a2c5394dfc03050ae96e316554b
Subhadip Das <subhadipdas911@gmail.com>
2023-08-23 02:18:42 +0530

    fix : fixed 2d Array initialization

--- Medium/subMatrixSumQueries.js
+++ Medium/subMatrixSumQueries.js
@@ -6,4 +6,34 @@ const E = [2, 3];
 
 // ! Function to calculate 2D Prefix Sum:-
 
+function prefix2DSum(matrix){
+    let rowCount = matrix.length;
+    let colCount = matrix[0].length;
 
+    console.log(rowCount,colCount);
+
+    // let prefixSum = new Array(rowCount).fill(new Array(colCount).fill(0));
+
+    // let prefixSum = [...Array(matrix.length)].map(() =>Array(matrix[0].length).fill(0));
+
+    const prefixSum = [];
+    for (let i=0; i<rowCount;i++){
+        const container = [];
+        for (let j=0;j<colCount;j++){
+            container.push(0);
+        }
+        prefixSum.push(container);
+    }
+    
+    for (let i=0;i<rowCount;i++){
+        console.log(matrix[i][0]);
+        prefixSum[i][0] += matrix[i][0];
+    }
+
+    for (let i=0; i<rowCount;i++){
+        console.log(prefixSum[i]);
+    }
+    return prefixSum;
+}   
+
+console.log(prefix2DSum(A));
