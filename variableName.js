9dbc5c9a3d82ba164fb4038f5f5458a7941b2ce3
Subhadip Das <subhadipdas911@gmail.com>
2023-09-19 11:44:02 +0530

    While Loop initializing

---
+++ variableName.js
@@ -0,0 +1,41 @@
+// let name = 'Subhadip';
+// let score = 90;
+// let standard = 'Cadet';
+
+// var name;
+// console.log(name);
+// var age = null;
+// name = "Subhadip";
+// console.log(age);
+// console.log(name);
+
+// let comment = "The new Apple Iphone\nhas been launched";
+// console.log(comment);
+
+// let commentOne = "The new phone is\tis on its way." // This will be a Tab String
+// console.log(commentOne); // This is the print statment.
+
+// This is a Single line comment.
+
+/* This will be a multiple line comment
+So there is no issue of misguiding */
+
+
+// console.log(~ (3>4));
+
+// console.log(10 && 8);
+
+// console.log((10 > 6) && (6 < 7));
+
+// console.log(0 || -29);
+
+// console.log(typeof(1) == typeof(NaN));
+
+// ! While Loop:-
+// let i = 0;
+
+// while (i <= 10){
+//     console.log(i);
+//     i+=1;
+// }
+// console.log('The end of the Loop');
