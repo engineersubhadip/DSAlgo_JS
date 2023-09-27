2db7518939fe1e13c3afded18bb16970c3bdd003
Subhadip Das <subhadipdas911@gmail.com>
2023-09-27 19:50:18 +0530

    practice JS

---
+++ dsPractice.js
@@ -0,0 +1,26 @@
+// // for (let i=0; i<rowCount; i++){
+//     //     matrix[i].sort((a,b) => a-b);
+//     // }
+    
+//     // for (let i=0; i<colCount; i++){
+//         //     matrix[i].sort((a,b) => a-b);
+//         // }
+//         // console.log(matrix);
+        
+// let matrix = [[13,4,12],[12,10,9]];
+// let rowCount = matrix.length;
+// let colCount = matrix[0].length;
+
+// function comparator(val1,val2){
+//     if (val1 <= val2){
+//         return -1;
+//     }else{
+//         return 1;
+//     }
+// }
+
+// for (let i=0 ; i<rowCount; i++){
+//     matrix.sort(comparator);
+// }
+
+// console.log(matrix);
