552a1fbc06d1cc917399fa0f2d3072816d014b04
Subhadip Das <subhadipdas911@gmail.com>
2023-09-29 12:25:36 +0530

    test : Code compilation success

--- Easy/painterPartitionProblem.js
+++ Easy/painterPartitionProblem.js
@@ -1,9 +1,46 @@
-let A = 10;
-let B = 1;
-let C = [1, 8, 11, 3];
+let A = 2;
+let B = 5;
+let C = [1, 10];
 
 for (let i=0; i<C.length; i++){
     C[i] = C[i] * B;
 }
 
-console.log(C);
+let left = Math.max(... C);
+let right = 0;
+for (let i=0; i<C.length; i++){
+    right+=C[i];
+}
+
+let ans = undefined;
+
+function checkScenario(array,midVal,target){
+    let workerCount = 1;
+    let workLoad = 0;
+    
+    for (let i=0; i<array.length; i++){
+        workLoad += array[i];
+        if (workLoad > midVal){
+            workerCount++;
+            workLoad = array[i];
+        }
+    }
+
+    if (workerCount > target){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+while (left <= right){
+    let mid = Math.floor((left+right)/2);
+    if (checkScenario(C,mid,A) == false){
+        left = mid+1;
+    }else{
+        ans = mid;
+        right = mid-1;
+    }
+}
+
+console.log(ans);
