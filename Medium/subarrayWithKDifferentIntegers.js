0752318909112c69ed497582773f3e232dddfe4d
Subhadip Das <subhadipdas911@gmail.com>
2023-10-02 13:33:10 +0530

    test : Code compilation success

---
+++ Medium/subarrayWithKDifferentIntegers.js
@@ -0,0 +1,56 @@
+let nums = [1,2,1,2,3];
+let k = 2;
+
+let hmLong = new Map();
+let hmShort = new Map();
+let i = 0;
+let j = 0;
+let right = 0;
+let count = 0;
+
+while (right < nums.length){
+    if (hmLong.has(nums[right]) == false){
+        hmLong.set(nums[right],1);
+    }else{
+        hmLong.set(nums[right],hmLong.get(nums[right])+1);
+    }
+
+    if (hmShort.has(nums[right]) == false){
+        hmShort.set(nums[right],1);
+    }else{
+        hmShort.set(nums[right],hmShort.get(nums[right])+1);
+    }
+
+    while (i <= right && hmLong.size > k){
+        hmLong.set(nums[i], hmLong.get(nums[i])-1);
+        if (hmLong.get(nums[i]) == 0){
+            hmLong.delete(nums[i]);
+        }
+        i+=1;
+    }
+
+    while (j <= right && hmShort.size > k){
+        hmShort.set(nums[j], hmShort.get(nums[j])-1);
+        if (hmShort.get(nums[j]) == 0){
+            hmShort.delete(nums[j]);
+        }
+        j+=1;
+    }
+
+    while (j <= right && hmShort.size == k){
+        if (hmShort.get(nums[j]) > 1){
+            hmShort.set(nums[j], hmShort.get(nums[j])-1);
+        }else{
+            break;
+        }
+        j+=1;
+    }
+
+    if (hmLong.size  == k && hmShort.size == k){
+        count += j-i+1;
+    }
+
+    right+=1;
+}
+
+console.log(count);
