c59ba57f29cef0917546e1433e64fc788a2d3519
Subhadip Das <subhadipdas911@gmail.com>
2023-10-07 09:36:04 +0530

    how to get the ascii value of a string

--- practice.js
+++ practice.js
@@ -4,4 +4,12 @@
 
 // console.log(x,y,z);
 
-console.log(Infinity == Infinity);
+let string = "ab";
+
+let result = 0;
+
+for (let i=0; i<string.length; i++){
+    console.log("ASCII VALUE IS ",string[i]," ",string[i].charCodeAt(0));
+}
+
+console.log(result);
