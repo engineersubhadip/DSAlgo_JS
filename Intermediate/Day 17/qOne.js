aee672fd4d19371b9fc92678a57afeb17d036b63
Subhadip Das <subhadipdas911@gmail.com>
2023-12-25 23:28:15 +0530

    Initial Commit

---
+++ Intermediate/Day 17/qOne.js
@@ -0,0 +1,7 @@
+// Count of elements
+
+// Given an array A of N integers. 
+// Count the number of elements that have at least 1 elements greater than itself.
+
+// 1 <= N <= 105
+// 1 <= A[i] <= 109
