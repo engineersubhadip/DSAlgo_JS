4643e49136f1bd5bd20386b61f789bbf74ba23d7
Subhadip Das <subhadipdas911@gmail.com>
2023-12-26 00:20:44 +0530

    Code Execution success

--- Intermediate/Day 17/qSix.js
+++ Intermediate/Day 17/qSix.js
@@ -2,4 +2,16 @@
 // You have to find the second largest element/value in the array 
 // or report that no such element exists.
 
-let A = [2, 1, 2];
+let A = [2, 1, 2, 6, 3, 6, 4];
+
+let maxElement = Math.max(...A);
+
+let secondLargest = -1;
+
+for (let i=0; i<A.length; i++){
+    if (A[i] > secondLargest && A[i] < maxElement){
+        secondLargest = A[i];
+    }
+}
+
+console.log(secondLargest);
