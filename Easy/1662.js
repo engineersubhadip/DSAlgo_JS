e0248b3a363e8b31490e8ab4e7346be301a4f07d
Subhadip Das <subhadipdas911@gmail.com>
2024-06-21 19:21:24 +0530

    Longest Increasing Subsequence Brute Force using Backtracking

--- Easy/1662.js
+++ Easy/1662.js
@@ -1,31 +1,5 @@
-const word1 = ["ab", "c"];
-const word2 = ["a", "bc"];
+let x = [];
 
-let finalizedOne = "";
+x[2] = 23;
 
-for (let i=0; i< word1.length; i++){
-    finalizedOne += word1[i];
-}
-
-let finalizedTwo = "";
-
-for (let i=0; i< word2.length; i++){
-    finalizedTwo += word2[i];
-}
-
-if (finalizedOne.length != finalizedTwo.length){
-    console.log(false);
-}
-
-let pointerOne = 0
-let pointerTwo = 0
-
-while ((pointerOne < finalizedOne.length) && (pointerTwo < finalizedTwo.length)){
-    if (finalizedOne[pointerOne] != finalizedTwo[pointerTwo]){
-        console.log(false);
-    }
-    pointerOne++;
-    pointerTwo++;
-}
-
-console.log(true);
+console.log(x);
--- practice.js
+++ practice.js
@@ -1,80 +1,32 @@
-let  A = [2,2,2];
+let arr = [7,7,7,7,7,7,7];
+let ans = undefined;
+let maxLength = 0;
 
-let hm = {};
-
-for (let i=0; i<A.length; i++)
+function backtracking(runArr,index,lastSeen)
 {
-    A[i] = Number(A[i]);
-    if (hm[A[i]] === undefined)
+    if (index >= arr.length)
     {
-        hm[A[i]] = 1;
-    }
-    else
-    {
-        hm[A[i]] += 1;
-    }
-}
-
-let ans = [];
-
-function custom(num)
-{
-    for (let i=1; i*i <= num; i++)
-    {
-        if (num % i === 0)
+        if (runArr.length > maxLength)
         {
-            if (i*i === num)
-            {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-function backtracking(runArr,index,lastSeen,hm)
-{
-    if (runArr.length === A.length)
-    {
-        let temp = [...runArr];
-        if (temp.length > 1) // lets say the A = [41]. Then temp would have been [41]
-        {
-            ans.push(temp);
+            let temp = [...runArr];
+            maxLength = temp.length;
+            ans = temp;
         }
         return;
     }
 
-    for (let key in hm)
+    // Take :-
+    if (lastSeen < arr[index])
     {
-        if (runArr.length === 0 && hm[key] > 0)
-        {
-            runArr.push(Number(key));
-            lastSeen.push(Number(key));
-            hm[key] -= 1;
-            backtracking(runArr,index+1,lastSeen,hm);
-            runArr.pop();
-            lastSeen.pop();
-            hm[key] += 1;
-        }
-        else if (runArr.length !== 0 && hm[key] > 0)
-        {
-            let lastNumber = lastSeen[lastSeen.length-1];
-            let currNumber = Number(key);
-            let status = custom(lastNumber+currNumber);
-            if (status)
-            {
-                runArr.push(currNumber);
-                lastSeen.push(currNumber);
-                hm[currNumber] -= 1;
-                backtracking(runArr,index+1,lastSeen,hm);
-                runArr.pop();
-                lastSeen.pop();
-                hm[currNumber] += 1;
-            }
-        }
+        runArr.push(arr[index]);
+        backtracking(runArr,index+1,arr[index]);
+        runArr.pop();
     }
+
+    // Not Take :-
+    backtracking(runArr,index+1,lastSeen);
 }
 
-backtracking([],0,[],hm);
+backtracking([],0,-1e9+7);
 
-console.log(ans);
+console.log("ans",ans,"maxLength",maxLength);
