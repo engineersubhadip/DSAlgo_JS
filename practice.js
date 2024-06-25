580845fee1494d8940570baf1baaefc50cd48ae6
Subhadip Das <subhadipdas911@gmail.com>
2024-06-25 12:43:26 +0530

    BS on Matrix

--- practice.js
+++ practice.js
@@ -1,17 +1,40 @@
-function recursion(arr,start,end)
+let A = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]];
+let B = 2;
+
+let row = A.length;
+let col = A[0].length;
+
+let currRow = 0;
+let currCol = col-1;
+let ans = 1e9+7;
+
+while (currRow < row && currCol >= 0)
 {
-    if (start >= end)
+    if (A[currRow][currCol] === B)
     {
-        return true;
+        let run = currCol;
+        while (run >= 0 && A[currRow][run] === B)
+        {
+            run -= 1;
+        }
+        run += 1;
+        let start = currRow+1;
+        let end = currCol + 1;
+        let runAns = (start * 1009) + end;
+        ans = Math.min(ans,runAns);
+        run -= 1;
+        currCol = run;
     }
-
-    if (arr[start] !== arr[end])
+    else if (A[currRow][currCol] < B)
     {
-        return false;
+        currRow += 1;
     }
-    return recursion(arr,start+1,end-1);
+    else if (A[currRow][currCol] > B)
+    {
+        currCol -= 1;
+    }
 }
-
-let ans = recursion("malayalam",0,"malaalam".length-1);
 
 console.log(ans);
