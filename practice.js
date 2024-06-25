8855cf54ffc99d2ff65744d6377882a6da74dbe1
Subhadip Das <subhadipdas911@gmail.com>
2024-06-25 12:52:29 +0530

    Longest Subarray with most zero Optimal Approach

--- practice.js
+++ practice.js
@@ -1,30 +1,56 @@
 let arr = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1];
 let k = 3;
-let maxLen = 0;
 
 
+let count = 0;
 for (let i=0; i<arr.length; i++)
 {
-    for (let j=i; j<arr.length; j++)
+    if (arr[i] === 1)
     {
-        let runArr = [];
-        for (let k=i; k<=j; k++)
-        {
-            runArr.push(arr[k]);
-        }
-        let runCount = 0;
-        for (let g = 0; g<runArr.length; g++)
-        {
-            if (runArr[g] === 0)
-            {
-                runCount += 1;
-            }
-        }
-        if (runCount <= k)
-        {
-            maxLen = Math.max(maxLen,runArr.length);
-        }
+        count += 1;
     }
+}
+
+if (count === arr.length)
+{
+    return count;
+}
+
+let maxLen = 0;
+let left = 0;
+let right = 0;
+let oneCount = 0;
+let zeroCount = 0;
+
+while (right < arr.length)
+{
+    if (arr[right] === 1) 
+    {
+        oneCount += 1;
+    }
+    else
+    {
+        zeroCount += 1;
+    }
+
+    while (left <= right && zeroCount > k)
+    {
+        if (arr[left] === 0)
+        {
+            zeroCount -= 1;
+        }
+        else
+        {
+            oneCount -= 1;
+        }
+        left += 1;
+    }
+
+    if (left <= right && zeroCount <= k && (oneCount + zeroCount > maxLen))
+    {
+        maxLen = oneCount + zeroCount;
+    }
+    right += 1;
 }
 
 console.log(maxLen);
