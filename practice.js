32cb609a26d9cdcfcdfb1cc9e0ab6244034e7168
Subhadip Das <subhadipdas911@gmail.com>
2024-06-25 10:36:29 +0530

    Printing Num from 1 to N :- Using Recursion

--- practice.js
+++ practice.js
@@ -1,32 +1,12 @@
-let arr = [7,7,7,7,7,7,7];
-let ans = undefined;
-let maxLength = 0;
-
-function backtracking(runArr,index,lastSeen)
+function recursion(num)
 {
-    if (index >= arr.length)
+    if (num === 1)
     {
-        if (runArr.length > maxLength)
-        {
-            let temp = [...runArr];
-            maxLength = temp.length;
-            ans = temp;
-        }
+        console.log(num);
         return;
     }
-
-    // Take :-
-    if (lastSeen < arr[index])
-    {
-        runArr.push(arr[index]);
-        backtracking(runArr,index+1,arr[index]);
-        runArr.pop();
-    }
-
-    // Not Take :-
-    backtracking(runArr,index+1,lastSeen);
+    recursion(num-1);
+    console.log(num);
 }
 
-backtracking([],0,-1e9+7);
-
-console.log("ans",ans,"maxLength",maxLength);
+recursion(5);
