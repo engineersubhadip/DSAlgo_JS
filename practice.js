b0a4d1ff2e27a096cc2ef4b9882b29a6e9be64ec
Subhadip Das <subhadipdas911@gmail.com>
2024-06-27 03:25:51 +0530

    Kth Triplet via Binary Search

--- practice.js
+++ practice.js
@@ -1,25 +1,58 @@
-// Count all the Prime Factors of a Number
+function custom(arr,refEle)
+{ 
+    // TC : O(N**2) and SC : O(1)
 
-let num = 19;
-let i = 2;
-let ans = 0;
+    let i = 0;
+    let count = 0;
 
-while (i * i <= num)
-{
-    if (num % i === 0)
+    while (i < arr.length)
     {
-        ans += 1;
-        while (num % i === 0)
+        let j = i+1;
+        let k = arr.length-1;
+        while (j < k)
         {
-            num = Math.floor(num/i);
+            if (arr[i] + arr[j] + arr[k] <= refEle)
+            {
+                count += (k - j);
+                j += 1;
+            }
+            else if (arr[i] + arr[j] + arr[k] > refEle)
+            {
+                k -= 1;
+            }
         }
+        i += 1;
     }
-    i += 1;
+    return count;
 }
 
-if (num > 1)
+// TC : O(N**2 * logN)  and SC : O(1)
+
+
+let arr = [2, 4, 3, 2];
+let res = [];
+let B = 3;
+
+arr.sort((a,b) => a-b);
+
+let left = arr[0]+arr[1]+arr[2];
+let right = arr[arr.length-1] + arr[arr.length-2] + arr[arr.length-3];
+let ans = undefined;
+
+while (left <= right)
 {
-    ans += 1;
+    let mid = Math.floor((left + (right-left)/2));
+    let runCount = custom(arr,mid);
+
+    if (runCount >= B)
+    {
+        ans = mid;
+        right = mid - 1;
+    }
+    else
+    {
+        left = mid + 1;
+    }
 }
 
 console.log(ans);
