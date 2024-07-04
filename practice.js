36fb618282385373d214150a29e7ec516d2e1fc4
Subhadip Das <subhadipdas911@gmail.com>
2024-07-04 23:07:34 +0530

    Printing the Even Numbers in a Range whose Set Bit Count is Prime Number

--- practice.js
+++ practice.js
@@ -1,10 +1,61 @@
-let arr = [1,2,3,4,5];
-let pfOR = new Array(arr.length).fill(0);
-pfOR[0] = arr[0];
+// Even Numbers whose SET Bit Count is Prime Numbers
+// 1 to 100
 
-for (let i=1; i<arr.length; i++)
+let arr = [];
+for (let i=1; i<=100; i++)
 {
-    pfOR[i] = pfOR[i-1] | arr[i];
+    arr.push(i);
 }
 
-console.log(pfOR);
+function checkBit(num,bit)
+{
+    if ((num & (1 << bit)) === 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+function isPrime(num)
+{
+    if (num <= 1)
+    {
+        return false;
+    }
+    for (let i=2; i*i <= num ; i++)
+    {
+        if (num % i === 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+let ans = [];
+
+for (let i=0; i<arr.length; i++)
+{
+    if (arr[i] % 2 === 0)
+    {
+        let runCount = 0;
+        for (let j=0; j<=31; j++)
+        {
+            let res = checkBit(arr[i],j);
+            if (res)
+            {
+                runCount += 1;
+            }
+        }
+        let primeStatus = isPrime(runCount);
+        if (primeStatus)
+        {
+            ans.push(arr[i]);
+        }
+    }
+}
+
+console.log(ans);
