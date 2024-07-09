33c6408a453647c564e0ee6625f291f7bce79706
Subhadip Das <subhadipdas911@gmail.com>
2024-07-09 14:57:21 +0530

    Implemented Backtracking to generate all the possible subsequences for both Strings

---
+++ Backtracking/BetterStringGFG.js
@@ -0,0 +1,54 @@
+// BetterStringGFG.js
+
+// Given a pair of strings of equal lengths, Geek wants to find the better string. The better string is the string having more number of distinct subsequences.
+// If both the strings have equal count of distinct subsequence then return str1.
+
+let str1 = "gfg";
+let str2 = "ggg";
+
+let ans = {};
+
+function backtracking(runArr,index,tarString)
+{
+    if (index >= tarString.length)
+    {
+        let temp = [...runArr];
+        temp = temp.join('');
+
+        if (ans[temp] === undefined && temp.length !== 0)
+        {
+            ans[temp] = 1;
+        }
+        return;
+    }
+
+    runArr.push(tarString[index]);
+    backtracking(runArr,index+1,tarString);
+    runArr.pop();
+
+    backtracking(runArr,index+1,tarString);
+}
+
+ans = {};
+
+backtracking([],0,str1);
+
+let res1 = [];
+for (let key in ans)
+{
+    res1.push(key);
+}
+
+ans = {};
+
+backtracking([],0,str2);
+
+let res2 = [];
+for (let key in ans)
+{
+    res2.push(key);
+}
+
+console.log("res1",res1);
+
+console.log("res2",res2)
