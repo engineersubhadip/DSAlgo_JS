95869420f904fdb947a95fbffc52104f6a9535e2
Subhadip Das <subhadipdas911@gmail.com>
2024-07-14 20:46:29 +0530

    Apple Frontend PS/DS : return an array where the each value is the product of the next two items

--- practice.js
+++ practice.js
@@ -1,12 +1,42 @@
-function gcd(a,b)
+let arr = [3, 4, 5];
+
+if (arr.length === 0)
 {
-    if (b === 0)
-    {
-        return a;
-    }
-    return gcd(b,a%b);
+    console.log([]);
 }
 
-let ans = (12*8)/gcd(12,8);
+let pfArr = new Array(arr.length).fill(0);
+pfArr[0] = arr[0];
 
-console.log(ans);
+for (let i=1; i<arr.length; i++)
+{
+    pfArr[i] = pfArr[i-1] * arr[i];
+}
+
+let sfArr = new Array(arr.length).fill(0);
+sfArr[sfArr.length-1] = arr[arr.length-1];
+
+for (let i=arr.length-2; i>=0; i--)
+{
+    sfArr[i] = sfArr[i+1] * arr[i];
+}
+
+let res = [];
+
+for (let i=0; i<arr.length; i++)
+{
+    if (i === 0)
+    {
+        res.push(sfArr[i+1]);
+    }
+    else if (i === arr.length-1)
+    {
+        res.push(pfArr[i-1]);
+    }
+    else
+    {
+        res.push(pfArr[i-1] * sfArr[i+1]);
+    }
+}
+
+console.log(res);
