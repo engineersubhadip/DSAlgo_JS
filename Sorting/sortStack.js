e693f11c291965ac2c1c472081db24960f8d4105
Subhadip Das <subhadipdas911@gmail.com>
2024-07-22 13:40:09 +0530

    Sorted a stack using a temporary stack

---
+++ Sorting/sortStack.js
@@ -0,0 +1,26 @@
+let stack = [34,3,31,98,92,23];
+
+let tempStack = [];
+
+while (stack.length > 0)
+{
+    let currEle = stack.pop();
+
+    if (tempStack.length === 0)
+    {
+        tempStack.push(currEle);
+    }
+    else
+    {
+        
+        while (tempStack.length > 0 && tempStack[tempStack.length-1] >= currEle)
+        {
+            let popEle = tempStack[tempStack.length-1];
+            tempStack.pop();
+            stack.push(popEle);
+        }
+
+        tempStack.push(currEle);
+    }
+}
+
