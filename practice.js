8581ab9509d138341b391d6ede99b311aa247ff5
Subhadip Das <subhadipdas911@gmail.com>
2024-08-02 18:42:14 +0530

    JavaScript function to print all objects from an array that share the same city name

--- practice.js
+++ practice.js
@@ -1,61 +1,54 @@
-let arr1 = [1,2,99,100,115];
-let arr2 = [2,5,99,5,1,5];
+let arr = [
+    {"name":"S1","city" : "C1", "pincode" : 123},
+    {"name":"S2","city" : "C1", "pincode" : 124},
+    {"name":"S3","city" : "C2", "pincode" : 125},
+    {"name":"S4","city" : "C1", "pincode" : 126},
+    {"name":"S5","city" : "C3", "pincode" : 127},
+    {"name":"S6","city" : "C3", "pincode" : 132},
+    {"name":"S7","city" : "C4", "pincode" : 112}
+]
 
-arr1.sort((a,b) => a-b);
-
-let set = new Set();
-
-for (let i=0; i<arr2.length; i++)
+function getObjects(arr,cityName)
 {
-    set.add(arr2[i]);
-}
-
-for (let i=0; i<arr1.length; i++)
-{
-    if (set.has(arr1[i]) === false)
+    let res = {};
+    
+    for (let i=0; i<arr.length; i++)
     {
-        console.log(arr1[i]);
-        break;
+        let currCity = arr[i].city;
+    
+        if (res[currCity] === undefined)
+        {
+            res[currCity] = [arr[i]];
+        }
+        else
+        {
+            res[currCity].push(arr[i]);
+        }
     }
+    
+    // console.log(res);
+    
+    let output = [];
+
+    for (let key in res)
+    {
+        if (key === cityName)
+        {
+            let arr = res[key];
+            for (let i=0; i<arr.length; i++)
+            {
+                output.push(arr[i]);
+            }
+        }
+    }
+
+    if (output.length === 0)
+    {
+        return "City Not Found";
+    }
+    return output;
 }
 
-console.log(-1);
-// arr2.sort((a,b) => a-b);
+let res = getObjects(arr,"C5");
 
-
-// let ans = -1;
-
-// let i = 0;
-// let j = 0;
-
-// while (i < arr1.length && j < arr2.length)
-// {
-//     if (arr1[i] === arr2[j])
-//     {
-//         let tempI = i;
-//         while (tempI < arr1.length && arr1[tempI] === arr1[i])
-//         {
-//             tempI += 1;
-//         }
-
-//         let tempJ = j;
-//         while (tempJ < arr2.length && arr2[tempJ] === arr2[j])
-//         {
-//             tempJ += 1;
-//         }
-
-//         i = tempI;
-//         j = tempJ;
-//     }
-//     else if (arr1[i] < arr2[j])
-//     {
-//         ans = arr1[i];
-//         break;
-//     }
-//     else if (arr2[j] < arr1[i])
-//     {
-//         j ++;
-//     }
-// }
-
-// console.log(ans);
+console.log(res);
