94dda793ab05696cc0bb6da379870668d39f3100
Subhadip Das <subhadipdas911@gmail.com>
2024-08-06 13:06:25 +0530

    How to efficiently add element in the array:- SalesForce

--- practice.js
+++ practice.js
@@ -1,38 +1,55 @@
-let str = "stataestanrstante";
-let tar = "stan";
+let arr = [1,2,3,4,5,6,7,8,9,10];
 
-let res = "";
-let i = 0;
+// * 1. Given index is at 0
+// * 2. Given index is at last
+// * 3. Given index is at middle
+// * 4. Given index is out of bounds
 
-while (i < str.length)
+// * Insert at 0th, last, middle, unbounded
+function validInsertion(arr,pos,ele)
 {
-    if (str[i] === tar[0])
+    if (pos > arr.length)
     {
-        let tempI = i;
-        let tempJ = 0;
-
-        while (tempI < str.length && tempJ < tar.length && str[tempI] === tar[tempJ])
+        return "Enter a valid Position...";
+    }
+    if (pos === 0)
+    {
+        let newArr = [];
+        newArr.push(ele);
+        for (let i=0; i<arr.length; i++)
         {
-            tempI += 1;
-            tempJ += 1;
+            newArr.push(arr[i]);
         }
 
-        if (tempJ === tar.length)
+        let lastEle = arr[arr.length-1];
+        for (let i=0; i<arr.length; i++)
         {
-            i = tempI;
+            arr[i] = newArr[i];
         }
-        else
-        {
-            res += str[i];
-            i += 1;
-        }
-        
+        arr.push(lastEle);
+        return arr;
+    }
+    else if (pos === arr.length)
+    {
+        arr.push(ele);
+        return arr;
     }
     else
     {
-        res += str[i];
-        i += 1;
+        // middle
+        let arr1 = arr.slice(0,pos);
+        let arr2 = arr.slice(pos,arr.length);
+        arr1.push(ele);
+        arr1 = arr1.concat(arr2);
+        for (let i=0; i<arr1.length; i++)
+        {
+            arr[i] = arr1[i];
+        }
+
+        return arr;
     }
 }
 
-console.log(res);
+let ans = validInsertion(arr,8,"Twenty");
+
+console.log(ans);
