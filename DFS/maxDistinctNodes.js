522ec136976965c96ce8a05cfca35bf47c411cce
Subhadip Das <subhadipdas911@gmail.com>
2024-08-24 11:10:05 +0530

    Initial Commit

--- DFS/maxDistinctNodes.js
+++ DFS/maxDistinctNodes.js
@@ -1,31 +1,51 @@
-let maxLength = 0;
+let arr = [[6,2],[5,2],[4,2]];
 
-function calculateMaxPath(root,hm)
-{
-    if (root === null)
+arr.sort(function(a,b){
+    let lvl1 = a[1];
+    let lvl2 = b[1];
+
+    if (lvl1 < lvl2)
     {
-        return;
+        return -1;
     }
-
-    if (hm[root.data] === undefined)
+    else if (lvl2 < lvl1)
     {
-        hm[root.data] = 1;
+        return 1;
     }
     else
     {
-        return;
+        if (a[0] <= b[0])
+        {
+            return -1;
+        }
+        return 1;
     }
+});
 
-    if (root.left === null && root.right === null)
-    {
-        let runLen = Object.keys(hm).length;
-        maxLength = Math.max(maxLength,runLen);
-        return;
-    }
 
-    calculateMaxPath(root.left,hm);
-    calculateMaxPath(root.right,hm);
+let ans = [];
+
+if (arr.length === 1) // [[x,y]]
+{
+    ans.push([arr[0][0]]);
 }
+else{
+    let prevLevel = arr[0][1];
+    let runAns = [arr[0][0]];
+    for (let i=1; i<arr.length; i++)
+    {
+        let currLevel = arr[i][1];
+        if (currLevel === prevLevel)
+        {
+            runAns.push(arr[i][0]);
+        }
+        else{
+            ans.push(runAns);
+            prevLevel = currLevel;
+            runAns = [arr[i][0]];
+        }
+    }
+    ans.push(runAns);
 
-calculateMaxPath(root,{});
-
+    console.log(ans);
+}
