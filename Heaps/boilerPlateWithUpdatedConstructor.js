42fba8ae54086c0c4bcceed6803386df1896d2bf
Subhadip Das <subhadipdas911@gmail.com>
2024-09-02 13:58:14 +0530

    Boiler Plate for Min Heap when comparison is made from 2 values

---
+++ Heaps/boilerPlateWithUpdatedConstructor.js
@@ -0,0 +1,84 @@
+// * Boiler Plate for MinHeap when constructor is made from 2 criterias
+class Heap {
+    constructor(comparator = function(a,b){
+        if (a[1] < b[1])
+        {
+            return true;
+        }
+        else if (b[1] < a[1])
+        {
+            return false;
+        }
+        else
+        {
+            if (a[2] < b[2])
+            {
+                return true;
+            }
+            else if (b[2] < a[2])
+            {
+                return false;
+            }
+            return true;
+        }
+    }) {
+        this._heap = [];
+        this._comparator = comparator;
+    }
+    size() {
+        return this._heap.length;
+    }
+    isEmpty() {
+        return this.size() == 0;
+    }
+    peek() {
+        return this._heap[top];
+    }
+    push(...values) {
+        values.forEach(value => {
+            this._heap.push(value);
+            this._siftUp();
+        });
+        return this.size();
+    }
+    pop() {
+        const poppedValue = this.peek();
+        const bottom = this.size() - 1;
+        if (bottom > top) {
+            this._swap(top, bottom);
+        }
+        this._heap.pop();
+        this._siftDown();
+        return poppedValue;
+    }
+    replace(value) {
+        const replacedValue = this.peek();
+        this._heap[top] = value;
+        this._siftDown();
+        return replacedValue;
+    }
+    _greater(i, j) {
+        return this._comparator(this._heap[i], this._heap[j]);
+    }
+    _swap(i, j) {
+        [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+    }
+    _siftUp() {
+        let node = this.size() - 1;
+        while (node > top && this._greater(node, parent(node))) {
+            this._swap(node, parent(node));
+            node = parent(node);
+        }
+    }
+    _siftDown() {
+        let node = top;
+        while (
+            (left(node) < this.size() && this._greater(left(node), node)) ||
+            (right(node) < this.size() && this._greater(right(node), node))
+        ) {
+            let maxChild = (right(node) < this.size() && this._greater(right(node), left(node))) ? right(node) : left(node);
+            this._swap(node, maxChild);
+            node = maxChild;
+        }
+    }
+}
