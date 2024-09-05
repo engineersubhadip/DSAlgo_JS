3f579150b60b9b67acc6b86bda7c87f5292371f0
Subhadip Das <subhadipdas911@gmail.com>
2024-09-05 17:15:49 +0530

    Implemented the Optimized Recursive approach for the question :- Zeta Frontend DP Ques

--- DP/1D/byteLandGoldCoin.js
+++ DP/1D/byteLandGoldCoin.js
@@ -1,19 +1,30 @@
 let num = 24;
+let dp = new Array(num+1).fill(-1);
 
-function dfs(num)
+function dfs(num,dp)
 {
     if (num <= 1)
     {
+        dp[num] = num;
         return num;
     }
 
-    let partOne = dfs(Math.floor(num/2));
-    let partTwo = dfs(Math.floor(num/3));
-    let partThree = dfs(Math.floor(num/4));
+    if (dp[num] !== -1)
+    {
+        return dp[num];
+    }
 
-    return Math.max(num,partOne+partTwo+partThree);
+    let partOne = dfs(Math.floor(num/2),dp);
+    let partTwo = dfs(Math.floor(num/3),dp);
+    let partThree = dfs(Math.floor(num/4),dp);
+
+    dp[num] = Math.max(num,partOne+partTwo+partThree);
+
+    return dp[num];
 }
 
-let ans = dfs(num);
+dfs(num,dp);
 
-console.log(ans);
+console.log("dp",dp);
+
+console.log(dp[num]);
