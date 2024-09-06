9a4bd1d4d7ad90803c0c0041d6f5e927b6f6e5e9
Subhadip Das <subhadipdas911@gmail.com>
2024-09-06 14:37:45 +0530

    Implemented the Memoized Solution using DP

--- DP/2D/totalWaysFromTLtoBR.js
+++ DP/2D/totalWaysFromTLtoBR.js
@@ -3,22 +3,36 @@ let mat = [[1,2,3],[4,5,6],[7,8,9]];
 let row = mat.length;
 let col = mat[0].length;
 
-function dfs(mat,currRow,currCol)
+let dp = new Array();
+
+for (let i=0; i<row; i++)
+{
+    let newArr = new Array(col).fill(-1);
+    dp.push(newArr);
+}
+
+function dfs(mat,currRow,currCol,dp)
 {
     if (currRow === 0 || currCol === 0)
     {
         return 1;
     }
 
+    if (dp[currRow][currCol] !== -1)
+    {
+        dp[currRow][currCol];
+    }
     // go top :-
-    let top = dfs(mat,currRow-1,currCol);
+    let top = dfs(mat,currRow-1,currCol,dp);
 
     // go left :-
-    let left = dfs(mat,currRow,currCol-1);
+    let left = dfs(mat,currRow,currCol-1,dp);
 
-    return top + left;
+    dp[currRow][currCol] = top + left;
+
+    return dp[currRow][currCol];
 }
 
-let ans = dfs(mat,row-1,col-1);
+let ans = dfs(mat,row-1,col-1,dp);
 
-console.log(ans);
+console.log(dp[row-1][col-1]);
