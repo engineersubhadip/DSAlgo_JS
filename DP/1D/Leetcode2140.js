2e31d01963644920568465ca92489e1b6cafdf3d
Subhadip Das <subhadipdas911@gmail.com>
2024-09-06 11:16:13 +0530

    Solved the question via the Brute Force Recursive approach

--- DP/1D/Leetcode2140.js
+++ DP/1D/Leetcode2140.js
@@ -1,2 +1,26 @@
-let arr = [[1,1],[2,2],[3,3],[4,4],[5,5]];
-console.log(arr);
+let arr = [[3,2],[4,3],[4,4],[2,5]];
+
+let maxSum = 0;
+
+function backtracking(arr,runSum,index)
+{
+    if (index >= arr.length)
+    {
+        maxSum = Math.max(maxSum,runSum);
+        return;
+    }
+
+    // pick :-
+    runSum += arr[index][0];
+    // console.log("runSum Before",runSum);
+    backtracking(arr,runSum,index+arr[index][1]+1);
+    runSum -= arr[index][0];
+    // console.log("runSum After",runSum);
+
+    // not pick:-
+    backtracking(arr,runSum,index+1);
+}
+
+backtracking(arr,0,0);
+
+console.log(maxSum);
