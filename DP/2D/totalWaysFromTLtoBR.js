767eb69e75ba4ee622c44d7100fb020e81fb86b6
Subhadip Das <subhadipdas911@gmail.com>
2024-09-06 14:35:03 +0530

    Implemented the Correct Brute Force Approach :- Recursive

--- DP/2D/totalWaysFromTLtoBR.js
+++ DP/2D/totalWaysFromTLtoBR.js
@@ -3,45 +3,22 @@ let mat = [[1,2,3],[4,5,6],[7,8,9]];
 let row = mat.length;
 let col = mat[0].length;
 
-let visited = new Array();
-
-for (let i=0; i<row; i++)
+function dfs(mat,currRow,currCol)
 {
-    let newArr = new Array(col).fill(false);
-    visited.push(newArr);
-}
-
-let ans = [];
-
-function dfs(mat,runArr,currRow,currCol,visited)
-{
-    if (currRow >= row || currCol >= col || visited[currRow][currCol] === true)
+    if (currRow === 0 || currCol === 0)
     {
-        return;
+        return 1;
     }
 
-    if (currRow === row-1 && currCol === col-1)
-    {
-        let temp = [...runArr];
-        temp = temp.join("");
-        ans.push(temp);
-    }
+    // go top :-
+    let top = dfs(mat,currRow-1,currCol);
 
-    visited[currRow][currCol] = true;
+    // go left :-
+    let left = dfs(mat,currRow,currCol-1);
 
-    // go right :-
-    runArr.push("R");
-    dfs(mat,runArr,currRow,currCol+1,visited);
-    runArr.pop();
-
-    // go down :-
-    runArr.push("D");
-    dfs(mat,runArr,currRow+1,currCol,visited);
-    runArr.pop();
-
-    visited[currRow][currCol] = false;
+    return top + left;
 }
 
-dfs(mat,[],0,0,visited);
+let ans = dfs(mat,row-1,col-1);
 
 console.log(ans);
