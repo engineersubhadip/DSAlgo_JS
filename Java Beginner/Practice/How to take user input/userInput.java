a81a59723a3502f68ddbf677a963ca1a3a7098cc
Subhadip Das <subhadipdas911@gmail.com>
2024-09-16 10:25:24 +0530

    Taking user input from User

---
+++ Java Beginner/Practice/How to take user input/userInput.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class userInput {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        System.out.print("input is "+ x);
+    }
+}
